bbf8c64cb83442d648cb7125babfc2b930de74b3
luisg1997 <41021395+luisg1997@users.noreply.github.com>
2020-08-09 17:17:18 -0600

    Rename Pulsos Binaurales/AnalisisEspectrodefrecuencias.m to Pulsos Binaurales/Codigo/Matlab/AnalisisEspectrodefrecuencias.m

--- Pulsos Binaurales/Codigo/Matlab/AnalisisEspectrodefrecuencias.m
+++ Pulsos Binaurales/Codigo/Matlab/AnalisisEspectrodefrecuencias.m
@@ -1,11 +1,11 @@
 %Leemos el archivo wav generado por el programa
 [y,fs] = audioread('Binaural0.wav');
-%Se verifica si hay un sonido estÈre (Buscamos N = 2)
+%Se verifica si hay un sonido est√©re (Buscamos N = 2)
 [M,N] = size(y);
 %Separamos ambos canales
 left_channel = y(:,1);
 right_channel = y(:,2);
-%Definimos el pulso binaural que se interpretarÌa 
+%Definimos el pulso binaural que se interpretar√≠a 
 binaural_beat= right_channel-left_channel;
 Fs = 44100;                    % Sampling frequency
 T = 1/Fs;                     % Sample time
@@ -17,6 +17,6 @@ f = Fs/2*linspace(0,1,NFFT/2+1);
 
 % Plot single-sided amplitude spectrum.
 plot(f,2*abs(Y(1:NFFT/2+1))) 
-title('An·lisis de Espectro de frecuencias Pulso Binaural')
+title('An√°lisis de Espectro de frecuencias Pulso Binaural')
 xlabel('Frecuencia (Hz)')
 ylabel('|Y(f)|')
