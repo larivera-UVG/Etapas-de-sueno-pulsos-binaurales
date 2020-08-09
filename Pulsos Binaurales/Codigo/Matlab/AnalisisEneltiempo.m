e70b96069746cdc5d1cbedb280b007bc3003f36b
luisg1997 <41021395+luisg1997@users.noreply.github.com>
2020-08-09 17:15:35 -0600

    Rename Pulsos Binaurales/AnalisisEneltiempo.m to Pulsos Binaurales/Codigo/Matlab/AnalisisEneltiempo.m

--- Pulsos Binaurales/Codigo/Matlab/AnalisisEneltiempo.m
+++ Pulsos Binaurales/Codigo/Matlab/AnalisisEneltiempo.m
@@ -1,11 +1,11 @@
 %Leemos el archivo wav generado por el programa
 [y,fs] = audioread('Binaural0.wav');
-%Se verifica si hay un sonido estére (Buscamos N = 2)
+%Se verifica si hay un sonido estÃ©re (Buscamos N = 2)
 [M,N] = size(y);
 %Separamos ambos canales
 left_channel = y(:,1);
 right_channel = y(:,2);
-%Definimos el pulso binaural que se interpretaría 
+%Definimos el pulso binaural que se interpretarÃ­a 
 binaural_beat= right_channel-left_channel;
 %Se hace un ajuste para tener la referencia en segundos
 t= (0:441000-1) / fs;
