ec2fdabe272b7568ce9a2036d068784ea1907b55
Oscar Lopez <kokalopez1997@hotmail.com>
2020-08-24 01:11:04 -0600

    correccion de errores
    Se arreglo la lectura en tiempo real, en donde el filtrado daba problemas en la separacion de epocas , ademas de agregar el capitulo de base de datos e introduccion a la tesis

--- Reconocimiento de Patrones/Codigo/Matlab/Tiempo Real/Live_Data.m
+++ Reconocimiento de Patrones/Codigo/Matlab/Tiempo Real/Live_Data.m
@@ -8,6 +8,8 @@ t=1:1000;
 vproyecto = zeros(1000,8);
 i = 1;
 ctrl = 0;
+ctrl2 = 1;
+M = 2;
 %% Se definen los parametros para los filtros
 fs_Hz = 250;
 little_buff = zeros(1,10);
@@ -23,25 +25,23 @@ notch = [59, 61];
 %%  Reservar memoria para vector de datos raw y para filtrar los datos
 Col = 8;
 Row = 50000000;
+Offset = zeros(1,8);
 Live_Data1 = zeros(Row,Col);
-Temp_Raw_Vector = zeros(Epocas_Length*100,8);
 Live_Data_test = [];
-Live_Data_test2 = [];
-Full_Fill_Vec = [];
-Proseced_Data = [];
-Feature_1 = [];
-Feature_2 = [];
-Feature_3 = [];
-Feature_4 = [];
-Feature_5 = [];
-Feature1_Vec = [];
-Feature2_Vec = [];
-Feature3_Vec = [];
-Feature4_Vec = [];
-Feature5_Vec = [];
-Feature6_Vec = [];
-Feature7_Vec = [];
-Feature8_Vec = [];
+Feature1_Vec = zeros(M,8);
+Feature2_Vec = zeros(M,8);
+Feature3_Vec = zeros(M,8);
+Feature4_Vec = zeros(M,8);
+Feature5_Vec = zeros(M,8);
+Live_Data_test2 = zeros(Epocas_Length*250,8);
+Fixed_Vector = zeros(Epocas_Length*250,8);
+Full_Fill_Vec = zeros(2*Epocas_Length*250,8);
+Temp_Raw_Vector = zeros(2*Epocas_Length*100,8);
+index1 = 1;
+cont = 1;
+cont2 = Epocas_Length*250;
+cont3 = Epocas_Length*250;
+tiempos = zeros(Epocas_Length*250,1);
 %% instantiate the library
 disp('Loading the library...');
 lib = lsl_loadlib();
@@ -57,58 +57,59 @@ disp('Opening an inlet...');
 inlet = lsl_inlet(result{1});
 disp('Now receiving data...');
 
-cont = 1;
-cont2 = Epocas_Length*250;
-while ctrl<=50
+
+mitimer  = tic;
+n = 1;
+
+
+while ctrl<=3
     
-    Epoc = [];
-    [vec,ts] = inlet.pull_sample(); % Se realiza la lectura de los 8 canales 
-    Live_Data_test2 = cat(1,Live_Data_test2,vec); % Cada lectura se almacena 
-    
-    display_buff_filt = filter(b2,a2,Live_Data_test2); % Se realiza el friltrado del los datos
-    display_buff_filt = filter(b,a,display_buff_filt);
+    [vec,ts] = inlet.pull_sample();                         % Se realiza la lectura de los 8 canales 
+    tiempos(n) = toc(mitimer);
+    mitimer  = tic;
+    n = n +1;
+    Live_Data_test2(index1,:) =  vec;                       % Se ingresa los datos leidos a un vector temporal
     
     i=i+1;
-    if(mod(i,Epocas_Length*250)==0) % Cada vez que pase cierta cantidad de tiempo se hace la separacion de epocas
+    if(mod(i,Epocas_Length*250+1)==0)                       % Cada vez que pase cierta cantidad de tiempo se hace la separacion de epocas
         i=1;
-        Pre_Epoc = display_buff_filt(cont:end ,:);
-        cont = cont + cont2;
-        Epoc = cat(1,Epoc,Pre_Epoc); 
-        flag = true;    % cuando se cumpla esta condicion se levanta una bandera para poder sacarle las caracterisiticas a dicha epoca
+        Fixed_Vector = Live_Data_test2 - (Offset);
+        display_buff_filt = filter(b2,a2,Fixed_Vector);  % Se realiza el friltrado del los datos
+        display_buff_filt = filter(b,a,display_buff_filt);
+        Offset = [mean(Live_Data_test2(:,1)),mean(Live_Data_test2(:,2)),mean(Live_Data_test2(:,3)),mean(Live_Data_test2(:,4)),mean(Live_Data_test2(:,5)),mean(Live_Data_test2(:,6)),mean(Live_Data_test2(:,7)),mean(Live_Data_test2(:,8))];
+        Full_Fill_Vec(cont:cont3 ,:) = Live_Data_test2;     % Se almacenan los datos de cada epoca de los valores en bruto
+        Temp_Raw_Vector(cont:cont3,:) = display_buff_filt;  % Se almacenan los datos de cada epoca de los valores filtrados
+        cont = cont + cont2;                                % Contador para la primera posicion de la proxima epoca a guardar
+        cont3 = cont3 + cont2;                              % Contador de la ultima posicion de la proxima epoca a guardar
+        index1 = 1;
+        flag = true;                                        % cuando se cumpla esta condicion se levanta una bandera para poder sacarle las caracterisiticas a dicha epoca
     else
         flag = false;
+        index1 = index1 + 1;
     end
    
     
     if flag == true
         %Extraer caracteristicas
-        Chn1 = Epoc(: , 1);
-        Chn2 = Epoc(: , 2);
-        Chn3 = Epoc(: , 3);
-        Chn4 = Epoc(: , 4);
-        Chn5 = Epoc(: , 5);
-        Chn6 = Epoc(: , 6);
-        Chn7 = Epoc(: , 7);
-        Chn8 = Epoc(: , 8);
+        Chn1 = display_buff_filt(: , 1);
+        Chn2 = display_buff_filt(: , 2);
+        Chn3 = display_buff_filt(: , 3);
+        Chn4 = display_buff_filt(: , 4);
+        Chn5 = display_buff_filt(: , 5);
+        Chn6 = display_buff_filt(: , 6);
+        Chn7 = display_buff_filt(: , 7);
+        Chn8 = display_buff_filt(: , 8);
         
-        clear Epoc;
         
-        Feature_1 = [mean(abs(Chn1)),mean(abs(Chn2)),mean(abs(Chn3)),mean(abs(Chn4)),mean(abs(Chn5)),mean(abs(Chn6)),mean(abs(Chn7)),mean(abs(Chn8))];
-        Feature_2 = [Zero_Crossing(Chn1,0.01),Zero_Crossing(Chn2,0.01),Zero_Crossing(Chn3,0.01),Zero_Crossing(Chn4,0.01),Zero_Crossing(Chn5,0.01),Zero_Crossing(Chn6,0.01),Zero_Crossing(Chn7,0.01),Zero_Crossing(Chn8,0.01)];
-        Feature_3 = [Min_Max_Distance(Chn1),Min_Max_Distance(Chn2),Min_Max_Distance(Chn3),Min_Max_Distance(Chn4),Min_Max_Distance(Chn5),Min_Max_Distance(Chn6),Min_Max_Distance(Chn7),Min_Max_Distance(Chn8)];
-        Feature_4 = [bandpower(Chn1),bandpower(Chn2),bandpower(Chn3),bandpower(Chn4),bandpower(Chn5),bandpower(Chn6),bandpower(Chn7),bandpower(Chn8)];
-        Feature_5 = [kurtosis(Chn1),kurtosis(Chn2),kurtosis(Chn3),kurtosis(Chn4),kurtosis(Chn5),kurtosis(Chn6),kurtosis(Chn7),kurtosis(Chn8)];
-        
-        % Cada caracteristica se almacena en vectores diferentes a medida
-        % que se extraen
-        Feature1_Vec = cat(1,Feature1_Vec,Feature_1);
-        Feature2_Vec = cat(1,Feature2_Vec,Feature_2);
-        Feature3_Vec = cat(1,Feature3_Vec,Feature_3);
-        Feature4_Vec = cat(1,Feature4_Vec,Feature_4);
-        Feature5_Vec = cat(1,Feature5_Vec,Feature_5);
+        Feature1_Vec(ctrl2,:) = [mean(abs(Chn1)),mean(abs(Chn2)),mean(abs(Chn3)),mean(abs(Chn4)),mean(abs(Chn5)),mean(abs(Chn6)),mean(abs(Chn7)),mean(abs(Chn8))];
+        Feature2_Vec(ctrl2,:) = [Zero_Crossing(Chn1,0.01),Zero_Crossing(Chn2,0.01),Zero_Crossing(Chn3,0.01),Zero_Crossing(Chn4,0.01),Zero_Crossing(Chn5,0.01),Zero_Crossing(Chn6,0.01),Zero_Crossing(Chn7,0.01),Zero_Crossing(Chn8,0.01)];
+        Feature3_Vec(ctrl2,:) = [Min_Max_Distance(Chn1),Min_Max_Distance(Chn2),Min_Max_Distance(Chn3),Min_Max_Distance(Chn4),Min_Max_Distance(Chn5),Min_Max_Distance(Chn6),Min_Max_Distance(Chn7),Min_Max_Distance(Chn8)];
+        Feature4_Vec(ctrl2,:) = [bandpower(Chn1),bandpower(Chn2),bandpower(Chn3),bandpower(Chn4),bandpower(Chn5),bandpower(Chn6),bandpower(Chn7),bandpower(Chn8)];
+        Feature5_Vec(ctrl2,:) = [kurtosis(Chn1),kurtosis(Chn2),kurtosis(Chn3),kurtosis(Chn4),kurtosis(Chn5),kurtosis(Chn6),kurtosis(Chn7),kurtosis(Chn8)];
         
         itr = 0;
-        disp('Done');
+        fprintf('Done - %d \n',ctrl);
         ctrl = ctrl + 1; % variable de control de duracion de la simulacion 
+        ctrl2 = ctrl2 + 1;
     end
 end
